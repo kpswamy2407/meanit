50191fc5cc6b9df81b052387f08e94fd45caa2d4
kpswamy540@gmail.com <kpswamy540@gmail.com>
2019-05-20 08:41:25 +0530

    random

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+const authHelper=require('../helpers').auth;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/random-number',(req,res)=>{
+	authHelper.getRandomString(3).then(randomString=>{
+		console.log(randomString);
+	 	if(randomString){
+	 		return res.json({random:randomString,status:200});
+	 	}
+	 	else{
+	 		return res.json({random:'657',status:201});
+	 	}
+	 })
 });
 
 module.exports = router;
